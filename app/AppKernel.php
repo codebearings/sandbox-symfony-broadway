d063e642a1fe4b58868ed8bb2075cff5818c7f80
codebearings <codebearings@gmail.com>
2017-01-14 13:23:56 +0100

    sandbox-symfony-broadway-1 Missing namespace in SandboxBundle.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Broadway\Bundle\BroadwayBundle\BroadwayBundle(),
-            new SandboxBundle(),
+            new SandboxBundle\SandboxBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/SandboxBundle/SandboxBundle.php
+++ src/SandboxBundle/SandboxBundle.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+namespace SandboxBundle;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
