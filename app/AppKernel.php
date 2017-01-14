6910a776bc3cb180f8f995de5554169371a30bee
codebearings <codebearings@gmail.com>
2017-01-14 13:12:16 +0100

    Add (and enable) SandboxBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Broadway\Bundle\BroadwayBundle\BroadwayBundle(),
+            new SandboxBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
